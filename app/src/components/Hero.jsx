67c02dabc8ae98a6a072ee6e7e8e7e5a52187999
elrobermtz <46224558+elrobermtz@users.noreply.github.com>
2026-04-24 19:28:45 -0600

    cambios logos

--- app/src/components/Hero.jsx
+++ app/src/components/Hero.jsx
@@ -5,7 +5,7 @@ export const Hero = () => {
     <section className="hero">
       <div className="hero-content">
         <img 
-          src="/logo.png" 
+          src="/logo-v3.png" 
           alt="Akredita Me" 
           className="hero-logo" 
         />
